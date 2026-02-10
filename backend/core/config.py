7b844f91cdb9f52b33545335cada589a387fc5a0
Anthony Lu <anthonylu7678@gmail.com>
2026-02-09 22:50:41 -0800

    Edit allowed origin

--- backend/core/config.py
+++ backend/core/config.py
@@ -30,7 +30,7 @@ class Settings:
         "http://localhost:5173",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
-        "https://releaf.vercel.app"
+        "https://releaf-city.vercel.app"
     ]
 
 
